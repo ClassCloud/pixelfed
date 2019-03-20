6a63b0eb065e744260a5703238e0e6441054dfd2
Patric Lougheed <xcentrikmedia@gmail.com>
2019-03-20 16:17:31 -0700

    Update 2019_03_20_000000_create_ldap_users_table.php

--- database/migrations/2019_03_20_000000_create_ldap_users_table.php
+++ database/migrations/2019_03_20_000000_create_ldap_users_table.php
@@ -1,3 +1,14 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
 public function up()
 {
     Schema::create('users', function (Blueprint $table) {
@@ -19,4 +30,14 @@ public function up()
         $table->rememberToken();
         $table->timestamps();
     });
+}
+/**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 }
